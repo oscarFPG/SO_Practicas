c7f17ea9f29e8a257fee3175badbd1483ebf4d7e
Oscar <oscarf1030p@gmail.com>
2024-10-23 20:41:13 +0000

    Casi hecho - falta mostrar informacion(Solo en el primer while)

--- P3/ejercicio4/espacio.c
+++ P3/ejercicio4/espacio.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/sysmacros.h>
@@ -8,8 +9,48 @@
 
 /* Forward declaration */
 void errorMessage(char* msg);
+int isActualOrPrevious(char* dirName);
+char* mergeNames(char* path, char* file);
 int get_size(char *fname, size_t *blocks);
 int get_size_dir(char *fname, size_t *blocks);
+
+
+/* 
+ * Processes all the files in the command line calling get_size on them to
+ * obtain the number of 512 B blocks they occupy and prints the total size in
+ * kilobytes on the standard output
+ */
+int main(int argc, char *argv[]){
+
+	struct stat info;
+	size_t blocks = 0;
+	int status;
+	if(argc == 1){	// No recibimos argumentos por la linea de comandos -> recorrer el directorio actual
+
+		status = get_size_dir(".", &blocks);
+		if(status == -1)
+			errorMessage("Error al recorrer el directorio");
+
+		printf("%dK   .\n", (int)blocks);
+	}
+	else{	// Mostrar el tamaño de los archivos pasados como parametro
+
+		char* path;
+		int i = 1;
+		while(i < argc){
+			
+			status = get_size(argv[i], &blocks);
+			if(status == -1)
+				errorMessage("Error al recorrer la lista de archivos");
+
+			printf("%dK   %s\n", (int)blocks, argv[i]);
+			++i;
+		}
+	}
+
+	return 0;
+}
+
 
 /**
  * Print an error message and exit the process
@@ -17,6 +58,41 @@ int get_size_dir(char *fname, size_t *blocks);
 void errorMessage(char* msg){
 	printf("%s\n", msg);
 	exit(EXIT_FAILURE);
+}
+
+/**
+ * Check if a directory is not "." or ".."
+ */
+int isActualOrPrevious(char* dirName){
+	return (strcmp(dirName, ".") == 0 || strcmp(dirName, "..") == 0) ? 1 : 0;
+}
+
+/**
+ * Merge the names in the format path\file
+ */
+char* mergeNames(char* path, char* file){
+
+	int newSize = strlen(path) + strlen(file) + 2;	// Size for both names and the '\' and '\0' terminator
+	char* newPath = malloc(sizeof(char) * newSize);
+	memset(newPath, 0, newSize);
+
+	int i = 0;
+	int firstSize = strlen(path);
+	while(i < firstSize){
+		newPath[i] = path[i];
+		++i;
+	}
+	newPath[i++] = '/';
+	
+	int j = 0;
+	int secondSize = strlen(file);
+	while(j < secondSize){
+		newPath[i] = file[j];
+		++i;
+		++j;
+	}
+
+	return newPath;
 }
 
 /* Gets in the blocks buffer the size of file fname using lstat. If fname is a
@@ -45,40 +121,57 @@ int get_size(char* fname, size_t* blocks){
  */
 int get_size_dir(char *dname, size_t *blocks){
 
+	DIR* dir = opendir(dname);
+	if(dir == NULL)
+		return -1;
 
-}
-
-/* Processes all the files in the command line calling get_size on them to
- * obtain the number of 512 B blocks they occupy and prints the total size in
- * kilobytes on the standard output
- */
-int main(int argc, char *argv[]){
-
+	// Primer recorrido de archivo para calcular el tamaño total del directorio
+	struct dirent* dirent;
 	struct stat info;
-	size_t blocks;
-	int status;
-	if(argc == 1){	// No se pasa nada por la linea de comandos -> recorrer el directorio actual
-
-		status = get_size_dir(".", &blocks);
-		if(status == -1)
-			errorMessage("Error al recorrer el directorio");
-
-		printf("%dK   .\n", blocks);
-	}
-	else{	// Mostrar el tamaño de los archivos pasados como parametro
-
-		char* path;
-		int i = 1;
-		while(i < argc){
-			
-			status = get_size(argv[i], &blocks);
-			if(status == -1)
-				errorMessage("Error al recorrer la lista de archivos");
-
-			printf("%dK   %s\n", blocks, argv[i]);
-			++i;
+	while( (dirent = readdir(dir)) != NULL ){
+		
+		char* filename = dirent->d_name;
+		if(isActualOrPrevious(filename))
+			continue;
+		
+		char* file = mergeNames(dname, filename);
+		if(lstat(file, &info) == -1){
+			free(file);
+			return -1;
 		}
+		free(file);
+
+		*blocks += info.st_size;
 	}
+	closedir(dir);
+
+	// Segundo recorrido para mostrar los posibles directorios anidados
+	DIR* dir2 = opendir(dname);
+	if(dir2 == NULL)
+		return -1;
+
+	size_t moreBlocks = 0;
+	while( (dirent = readdir(dir2)) != NULL ){
+		
+		char* filename = dirent->d_name;
+		if(isActualOrPrevious(filename))
+			continue;
+
+		char* dirName = mergeNames(dname, filename);
+		if(lstat(dirName, &info) == -1){
+			free(dirName);
+			return -1;
+		}
+
+		if(S_ISDIR(info.st_mode)){
+			if(get_size_dir(dirName, &moreBlocks) == -1){
+				free(dirName);
+				errorMessage("Se produjo un error al recorrer un directorio");
+			}
+		}
+		free(dirName);
+	}
+	closedir(dir2);
 
 	return 0;
 }
