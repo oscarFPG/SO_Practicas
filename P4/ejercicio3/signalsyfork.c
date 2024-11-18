658ee00d553bf1f14159879cb7ca2411012db13d
oscarFPG <95655329+oscarFPG@users.noreply.github.com>
2024-11-18 16:42:06 +0000

    Practica 4 terminada

--- P4/ejercicio3/signalsyfork.c
+++ P4/ejercicio3/signalsyfork.c
@@ -1,11 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+#define NUM_SECONDS 5
 
 /* Programa que temporiza la ejecución de un proceso hijo */
+void configureSignal(int signal, siginfo_t* info, void* context){
 
+	if(signal == SIGALRM)
+		kill(0, SIGKILL);
+}
+
+char **parse_command(const char *cmd, int* argc) {
+
+    // Allocate space for the argv array (initially with space for 10 args)
+    size_t argv_size = 10;
+    const char *end;
+    size_t arg_len; 
+    int arg_count = 0;
+    const char *start = cmd;
+    char **argv = malloc(argv_size * sizeof(char *));
+
+    if (argv == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    while (*start && isspace(*start)) start++; // Skip leading spaces
+
+    while (*start) {
+        // Reallocate more space if needed
+        if (arg_count >= argv_size - 1) {  // Reserve space for the NULL at the end
+            argv_size *= 2;
+            argv = realloc(argv, argv_size * sizeof(char *));
+            if (argv == NULL) {
+                perror("realloc");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        // Find the start of the next argument
+        end = start;
+        while (*end && !isspace(*end)) end++;
+
+        // Allocate space and copy the argument
+        arg_len = end - start;
+        argv[arg_count] = malloc(arg_len + 1);
+
+        if (argv[arg_count] == NULL) {
+            perror("malloc");
+            exit(EXIT_FAILURE);
+        }
+        strncpy(argv[arg_count], start, arg_len);
+        argv[arg_count][arg_len] = '\0';  // Null-terminate the argument
+        arg_count++;
+
+        // Move to the next argument, skipping spaces
+        start = end;
+        while (*start && isspace(*start)) start++;
+    }
+
+    argv[arg_count] = NULL; // Null-terminate the array
+
+    (*argc) = arg_count; // Return argc
+
+    return argv;
+}
 
 int main(int argc, char **argv){
+
+	if(argc != 2){
+		printf("Error: Introduce un comando para ejecutar\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int commandSize;
+	char** command = parse_command(argv[1], &commandSize);
+
+	// Configure signals
+	struct sigaction st;
+	st.sa_handler = configureSignal;
+	st.sa_flags = 0;
+	if( sigaction(SIGALRM, &st, NULL) == -1 ){
+		printf("Error al configurar el envio de signals\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	pid_t pid = fork();
+	if(pid == 0){
+		int status = execvp(command[0], command);
+		if(status == -1){
+			printf("Error al ejecutar el comando %s\n", command[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else{
+
+		// Start the alarm
+		alarm(NUM_SECONDS);
+
+		// Wait for the child process
+		int status;
+		wait(&status);
+		printf("Child process terminated with status %d\n", status);
+
+		// Free memory
+		for(int i = 0; 0 < commandSize; i++)
+			free(command[i]);
+		free(command);
+	}
 
 	return 0;
 }
--- P4/ejercicio4/inicial.c
+++ P4/ejercicio4/inicial.c
@@ -6,16 +6,21 @@
 #include <sys/wait.h>
 #include <string.h>
 
-int main(void)
-{
-    int fd1,fd2,i,pos;
+#define OPCION_A 0
+#define OPCION_B 1
+
+void opcionA(){
+
+    int fd1, fd2, pos;
     char c;
     char buffer[6];
+    memset(&buffer, 0, 6);
 
-    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
+    fd1 = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR, 0664);
     write(fd1, "00000", 5);
-    for (i=1; i < 10; i++) {
-        pos = lseek(fd1, 0, SEEK_CUR);
+    for (int i = 1; i < 10; i++) {
+
+        pos = lseek(fd1, 5*i, SEEK_SET);
         if (fork() == 0) {
             /* Child */
             sprintf(buffer, "%d", i*11111);
@@ -25,7 +30,7 @@ int main(void)
             exit(0);
         } else {
             /* Parent */
-            lseek(fd1, 5, SEEK_CUR);
+            lseek(fd1, 0, SEEK_SET);
         }
     }
 
@@ -38,5 +43,55 @@ int main(void)
         printf("%c", (char) c);
     printf("\n");
     close(fd1);
-    exit(0);
+}
+
+/*
+    Proceso padre escribe en las posiciones 10 * i -> Siendo i un indice del 0 al 8
+    Proceso hijo i -> el primero en la posicion 5 y el resto en la posicion 10(i - 1) + 5
+*/
+void opcionB(){
+
+    int fd1, fd2;
+    char c;
+    char buffer[6];
+    memset(&buffer, 0, 6);
+
+    fd1 = open("outputB.txt", O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR, 0664);
+    write(fd1, "00000", 5);
+    for (int i = 1; i < 10; i++) {
+
+        /* Child */
+        if (fork() == 0) {
+            lseek(fd1, 10*(i - 1) + 5, SEEK_SET);
+            sprintf(buffer, "%d", i*11111);
+            write(fd1, buffer, 5);
+            close(fd1);
+            exit(0);
+        }
+        /* Parent */
+        else {
+            lseek(fd1, 10*(i - 1), SEEK_SET);
+            write(fd1, "00000", 5);
+        }
+    }
+
+	//wait for all childs to finish
+    while (wait(NULL) != -1);
+
+    lseek(fd1, 0, SEEK_SET);
+    printf("File contents are:\n");
+    while (read(fd1, &c, 1) > 0)
+        printf("%c", (char) c);
+    printf("\n");
+    close(fd1);
+}
+
+int main(void){
+    
+    if(OPCION_A)
+        opcionA();
+    if(OPCION_B)
+        opcionB();
+
+    return 0;
 }
