a55935b3f859dc37c4c7024b7ffe3cdddc429a81
oscarFPG <95655329+oscarFPG@users.noreply.github.com>
2024-11-26 22:10:23 +0000

    ???

--- P5/disco.c
+++ P5/disco.c
@@ -12,46 +12,55 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;				// Mutex
 pthread_cond_t firstStart = PTHREAD_COND_INITIALIZER;			// Condition variable for first start all at the same time
+int aforo = 0;													// Counter for people INSIDE the disco
+
 pthread_cond_t vipClientsWait = PTHREAD_COND_INITIALIZER;		// Wait for the vips -> Only if its full
-pthread_cond_t normalClientsWait = PTHREAD_COND_INITIALIZER;	// Wait for the normal clients -> If its full or it is some vip waiting on queue
 int vipClientsCurrentTurn = 0, vipClientsOnQueue = 0;			// Vip clients turn and max counter
+
+pthread_cond_t normalClientsWait = PTHREAD_COND_INITIALIZER;	// Wait for the normal clients -> If its full or it is some vip waiting on queue
 int normalClientsCurrentTurn = 0, normalClientsOnQueue = 0;		// Normal clients turn and max counter
-int clientsInsideDisco = 0;										// Counter for people INSIDE the disco
 
 typedef struct{
 	int clientType;		// 1 for vip, 0 for the rest
 	int turn;			// Turn number of this client
 } tCliente;
 
+
 void errorMessage(char* message){
 	printf("%s\n", message);
 	exit(EXIT_FAILURE);
 }
 
-
 void enter_normal_client(int id){
 
 	pthread_mutex_lock(&mutex);
-		while(vipClientsOnQueue != 0 || clientsInsideDisco != CAPACITY || normalClientsCurrentTurn != id){
+		while(vipClientsOnQueue != 0 || aforo != CAPACITY || normalClientsCurrentTurn != id)
 			pthread_cond_wait(&normalClientsWait, &mutex);
-		}
 		
 		printf("Normal client with turn %d entering the disco\n", id);
-		clientsInsideDisco++;
+		aforo++;
 		normalClientsCurrentTurn++;
 	pthread_mutex_unlock(&mutex);
 }
 
 void enter_vip_client(int id){
 
+	printf("Vip client with turn %d waiting on the line\n", id);
 	pthread_mutex_lock(&mutex);
-		while(clientsInsideDisco != CAPACITY || vipClientsCurrentTurn != id){
+		
+		vipClientsOnQueue++;
+		while(id != vipClientsCurrentTurn || aforo == CAPACITY)
 			pthread_cond_wait(&vipClientsWait, &mutex);
-		}
-
-		printf("Vip client with turn %d entering the disco\n", id);
-		clientsInsideDisco++;
+		
+		vipClientsOnQueue--;
 		vipClientsCurrentTurn++;
+		aforo++;
+		
+		// Wake up normal clients from here ONLY if there is no more vip clients
+		printf("Vip client with turn %d entering the disco\n", id);
+		printf("Now the turn is %d and the people inside the disco is %d\n", vipClientsCurrentTurn, aforo);
+		pthread_cond_broadcast(&vipClientsWait);
+
 	pthread_mutex_unlock(&mutex);
 }
 
@@ -63,13 +72,10 @@ void dance(int id, int isvip){
 void disco_exit(int id, int isvip){
 
 	pthread_mutex_lock(&mutex);
-		printf("Client %s with turn %d exiting the disco!\n", VIPSTR(isvip), id);
-		clientsInsideDisco--;
+		--aforo;
+		pthread_cond_broadcast(&vipClientsWait);
 	pthread_mutex_unlock(&mutex);
-	
-	pthread_cond_broadcast(&vipClientsWait);
-	pthread_cond_broadcast(&normalClientsWait);
-	pthread_exit(NULL);
+	printf("Client %s with turn %d exiting the disco!\n", VIPSTR(isvip), id);
 }
 
 void* client(void* arg){
@@ -95,6 +101,7 @@ void* client(void* arg){
 	// Once inside the disco -> dance and leave
 	dance(client.turn, client.clientType);
 	disco_exit(client.turn, client.clientType);
+	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]){
@@ -108,23 +115,16 @@ int main(int argc, char *argv[]){
 	file = fopen(FILENAME, "r");
 	if(file == NULL)
 		errorMessage("File not found");
-	if(DEBUG)
-		printf("File %s opened!\n", FILENAME);
-
 
 	// Read number of clients in the file
 	if(fscanf(file, "%d", &numberOfClients) == -1)
 		errorMessage("Number of clients cannot be read");
-	if(DEBUG)
-		printf("%d clients\n", numberOfClients);
-
 
 	// Read and create threads for each client
 	clientes = malloc(numberOfClients * sizeof(tCliente));
 	hilos = malloc(numberOfClients * sizeof(pthread_t));
 	for(int i = 0; i < numberOfClients; i++){
 		
-		// Read client type and assign turn
 		fscanf(file, "%d", &clientes[i].clientType);
 		if(clientes[i].clientType == 1)
 			clientes[i].turn = vipClientsOnQueue++;
@@ -137,12 +137,18 @@ int main(int argc, char *argv[]){
 
 	// Wake up all the clients on the line at the same time
 	pthread_cond_broadcast(&firstStart);
+	if(DEBUG)
+		printf("Wake up all threads\n");
 
 	// Wait for all threads
 	for(int i = 0; i < numberOfClients; i++)
 		pthread_join(hilos[i], NULL);
 
 	// Free memory and resources
+	pthread_mutex_destroy(&mutex);
+	pthread_cond_destroy(&firstStart);
+	pthread_cond_destroy(&vipClientsWait);
+	pthread_cond_destroy(&normalClientsWait);
 	free(clientes);
 	fclose(file);
 
